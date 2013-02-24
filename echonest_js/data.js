4c795f60c96e956538a6e252324d5241b0f3f66a
Your Name Here <Brendan.Conron@tufts.edu>
2013-02-23 20:46:29 -0500

    Added artists request functions

--- echonest_js/data.js
+++ echonest_js/data.js
@@ -1,7 +1,37 @@
+function run(){
+	jsonGenres = get_genres();
+	genresObj = JSON.parse(jsonGenres);
+	for(var i = 0; i < genresObj.response.genres.length; i++){
+		artstsJsonForGenre = request_artists(genresObj.response.genres["name"]);
+	}
+	//console.log(genresObj.response.genres[1]["name"]);
+	/*for(var i = 0; i < genresObj.length; i++){
+		artistJSON = request_artists(genresObj[0].name);
+	}*/	
+	//artObj = JSON.parse(artistJSON);
+	/*for(var i = 0; i < artObj.length; i++){
+		console.log(artObj[i]["name"]);
+	}*/
+}
+
+/*Returns JSON object with all genres*/
 function get_genres(){
-    console.log($.ajax({
+    return $.ajax({
         type: "GET",
         url: "http://developer.echonest.com/api/v4/artist/list_genres?api_key=G3SVBYIVLZTGEECOA&format=json",
         async: false,
-    }).responseText);
+    }).responseText;
 }
+
+/*Returns JSON object of all artists for specified genere*/
+function request_artists(genre){
+	url = "http://developer.echonest.com/api/v4/artist/search?api_key=G3SVBYIVLZTGEECOA&format=json&genre=";
+	url += genre;
+	return $.ajax({
+        type: "GET",
+        url: url,
+        async: false,
+    }).responseText;  
+}
+
+
