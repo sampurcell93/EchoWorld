a689e32c726103b72c21481ce5405dceea2a2893
Your Name Here <Brendan.Conron@tufts.edu>
2013-02-23 22:16:18 -0500

    Extract metadata from mp3 and request data

--- echonest_js/data.js
+++ echonest_js/data.js
@@ -1,36 +1,34 @@
-/*testing function*/
-function run(){
-	jsonGenres = get_genres();
-	genresObj = JSON.parse(jsonGenres);
-	console.log(genresObj);
-	for(var i = 0; i < genresObj.response.genres.length; i++){
-		artstsJsonForGenre = request_artists(genresObj.response.genres[i]["name"]);
-		artists = JSON.parse(artstsJsonForGenre);
-		for(var j = 0; j < artists.response.artists.length; j++){
-			console.log(artists.response.artists[j]["name"]);	
-		}	
+$(document).ready(function(){
+	song = requestEchoData("Kanye West", "All of the Lights");
+	song = JSON.parse(song);
+	id = song.response.songs[0].id;
+	songData = getSongData(id);
+	console.log(songData);
+	/*var file = "test.mp3";
+	function getData(file) {
+		artist = ID3.getTag(file, "artist");
+ 		title = ID3.getTag(file, "title");
+ 		requestEchoData(artist, title);
 	}
+	ID3.loadTags(file, getTitle);*/
+});
 
-}
-
-/*Returns JSON object with all genres*/
-function get_genres(){
-    return $.ajax({
+function requestEchoData(artist, title){
+	url = "http://developer.echonest.com/api/v4/song/search?api_key=G3SVBYIVLZTGEECOA&artist=" + artist + "&title=" + title;
+	return $.ajax({
         type: "GET",
-        url: "http://developer.echonest.com/api/v4/artist/list_genres?api_key=G3SVBYIVLZTGEECOA&format=json",
+        url: url, 
         async: false,
     }).responseText;
 }
 
-/*Returns JSON object of all artists for specified genere*/
-function request_artists(genre){
-	url = "http://developer.echonest.com/api/v4/artist/search?api_key=G3SVBYIVLZTGEECOA&format=json&genre=";
-	url += genre;
+function getSongData(id){
+	url = "http://developer.echonest.com/api/v4/song/profile?api_key=G3SVBYIVLZTGEECOA&id=" + id  + "&bucket=audio_summary";
 	return $.ajax({
         type: "GET",
-        url: url,
+        url: url, 
         async: false,
-    }).responseText;  
+    }).responseText;
 }
 
 
