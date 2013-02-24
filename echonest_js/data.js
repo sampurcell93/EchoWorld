778df7614278bfb4de0a739d7d73d3fa7d98c3e1
Your Name Here <Brendan.Conron@tufts.edu>
2013-02-23 22:21:43 -0500

    Modularized

--- echonest_js/data.js
+++ echonest_js/data.js
@@ -1,17 +1,17 @@
-$(document).ready(function(){
-	song = requestEchoData("Kanye West", "All of the Lights");
-	song = JSON.parse(song);
-	id = song.response.songs[0].id;
-	songData = getSongData(id);
-	console.log(songData);
-	/*var file = "test.mp3";
+function getAttributes(fileName){
+	var file = fileName;
+	songData = "";
 	function getData(file) {
 		artist = ID3.getTag(file, "artist");
  		title = ID3.getTag(file, "title");
- 		requestEchoData(artist, title);
+ 		song = requestEchoData(artist, title);
+ 		song = JSON.parse(song);
+		id = song.response.songs[0].id;
+		songData = getSongData(id);
 	}
-	ID3.loadTags(file, getTitle);*/
-});
+	ID3.loadTags(file, getTitle);
+	return songData;
+}
 
 function requestEchoData(artist, title){
 	url = "http://developer.echonest.com/api/v4/song/search?api_key=G3SVBYIVLZTGEECOA&artist=" + artist + "&title=" + title;
