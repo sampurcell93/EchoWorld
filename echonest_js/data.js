78fd54e1571accf26486f19585a09e2e560f4639
Your Name Here <Brendan.Conron@tufts.edu>
2013-02-23 19:18:45 -0500

    Added html testing template

--- echonest_js/data.js
+++ echonest_js/data.js
@@ -1,13 +1,7 @@
-<script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
-<script src="http://code.jquery.com/jquery-migrate-1.1.1.min.js"></script>
-
-function getRemote() {
+function get_genres(){
     console.log($.ajax({
         type: "GET",
         url: "http://developer.echonest.com/api/v4/artist/list_genres?api_key=G3SVBYIVLZTGEECOA&format=json",
         async: false,
     }).responseText);
-
 }
-
-getRemote();
